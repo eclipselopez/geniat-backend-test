11415c022607e0a95e5bc665daf6f3f528a85c58
eclipselopez <salvador-lopez.96@hotmail.com>
2023-02-16 04:21:46 -0600

    fix: remove comment into logger.ts

--- lib/logger.ts
+++ lib/logger.ts
@@ -2,8 +2,7 @@ import pino from 'pino'
 import config from 'config'
 
 const streams = [
-    { stream: process.stdout },
-    // { stream: pino.destination(`./logs/${config.get('api.identifier')}-${Date.now()}.log`)}
+    { stream: process.stdout }
 ]
 
 const options = (config.has('logger.options')) ? Object.assign({}, config.get('logger.options')) : {};
